431018516a01ef4ec5d3772d272ad9652bf54891
patodro <patodro@gmail.com>
2025-07-29 13:36:32 -0400

    Step 1 of Learn Flask

--- BasicProject/app.py
+++ BasicProject/app.py
@@ -3,17 +3,19 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
-
+# URL: /hello/<name>?message=I%20love%20Josh%20Allen
 @app.route('/')
-def hello():
+@app.route('/hello/<name>')
+def hello(name):
     """Renders a sample page."""
-    return "Hello World!"
+    msg = request.args.get('message','')
+    return "Hello "+ name + "! " + msg + "."
 
 if __name__ == '__main__':
     import os
